e4f5a058405bbd9a5ceebc91c1d412005c329ecf
John <jparkteach@yahoo.com>
2017-03-27 20:54:43 -0400

    InClassAssignment08

--- app/src/main/java/net/jptechnology/android/inclassassignment08_johnp/MainActivity.java
+++ app/src/main/java/net/jptechnology/android/inclassassignment08_johnp/MainActivity.java
@@ -1,7 +1,14 @@
 package net.jptechnology.android.inclassassignment08_johnp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new DownloadImageTask((ImageView) findViewById(R.id.imageView1))
+                .execute("gs://takehomeassignment08.appspot.com/userpics/0dc67f6d-6f68-415e-8b39-47b749900e95.png");
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
